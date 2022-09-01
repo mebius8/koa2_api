4b4c6dd2085c06c6a298fd2f101a411fa2ec477f
fushaoshan <317024297@qq.com>
2022-09-01 15:08:50 +0800

    koa起始框架搭建

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const Koa = require('koa')
+const app = new Koa()
+
+app.use((ctx, next) => {
+    ctx.body = 'hello world'
+})
+
+app.listen(3000,() =>{
+    console.log('server is running on http://localhost:3000')
+})
