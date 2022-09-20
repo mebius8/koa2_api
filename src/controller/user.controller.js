16e6d1db2eade47a9c70892b5c2e7ce1010bbb42
fushaoshan <317024297@qq.com>
2022-09-20 17:25:22 +0800

    注册接口错误处理

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,14 +1,42 @@
-const { createUser } = require('../service/user.service')
+const { createUser,getUerInfo } = require('../service/user.service')
 
 class UserController {
     async register(ctx, next) {
         // 1. 获取数据
         // console.log(ctx.request.body)
         const { user_name, password } = ctx.request.body
+
+        if(!user_name || !password){
+            ctx.status = 400
+            ctx.body = {
+                code: '10001',
+                message: '用户名或密码为空',
+                result: ''
+            }
+            return
+        }
+
+        if (await getUerInfo({user_name})) {
+            ctx.status = 409
+            ctx.body = {
+                code: '10002',
+                message: '用户已经存在',
+                result: '',
+            }
+            return
+        }
+
         // 2. 操作数据库
         const res = await createUser(user_name, password)
         // 3. 返回结果
-        ctx.body = res
+        ctx.body = {
+            code: 0,
+            message: '用户注册成功',
+            result: {
+                id: res.id,
+                user_name: res.user_name,
+            },
+        }
     }
 
     async login(ctx, next) {
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -5,6 +5,22 @@ class UserService{
         const res = await User.create({user_name, password})
         return res
     }
+
+    async getUerInfo({id, user_name, password, is_admin}){
+        const whereOpt = {}
+
+        id && Object.assign(whereOpt, { id })
+        user_name && Object.assign(whereOpt,{user_name})
+        password && Object.assign((whereOpt,{password}))
+        is_admin && Object.assign(whereOpt,{is_admin})
+
+        const res = await User.findOne({
+            attributes: ['id', 'user_name', 'password', 'is_admin'],
+            where: whereOpt
+        })
+
+        return res ? res : null
+    }
 }
 
 module.exports = new UserService()
