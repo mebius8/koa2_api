c1e7d3eb2c24b55587c48303ff58a64c07cbe6ed
fushaoshan <317024297@qq.com>
2022-09-27 15:19:52 +0800

    完善错误处理中间件

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,12 @@
 const Koa = require('koa')
 const KoaBody = require('koa-body')
 const userRouter = require('../router/user.route')
+const errHandler = require('./errHandler')
 
 const app = new Koa()
 //KoaBody先注册
 app.use(KoaBody())
-
 app.use(userRouter.routes())
+app.on('error', errHandler)
 
 module.exports = app
---
+++ src/middleware/user.middleware.js
@@ -0,0 +1,44 @@
+const {getUerInfo} = require('../service/user.service')
+const {userFormateError, userAlreadyExited, userRegisterError} = require('../constant/err.type')
+
+const userValidator = async (ctx, next) => {
+    const {user_name, password} = ctx.request.body
+    if (!user_name || !password) {
+        console.error('用户名或密码为空', ctx.request.body)
+        ctx.app.emit('error', userFormateError, ctx)
+        return
+    }
+
+    await next()
+}
+
+const verifyUser = async (ctx, next) => {
+    const {user_name} = ctx.request.body
+    // if (await getUerInfo({user_name})) {
+    //     ctx.app.emit('error', userAlreadyExited, ctx)
+    //     return
+    // }
+
+    try {
+        const res = await getUerInfo({user_name})
+        if(res){
+            console.error('用户名已经存在', {user_name})
+            ctx.app.emit('error', userAlreadyExited, ctx)
+            return
+        }
+    }catch (err){
+        console.log('获取用户信息错误', err)
+        ctx.app.emit('error', userAlreadyExited, ctx)
+        return
+    }
+
+    await next()
+}
+
+
+module.exports = {
+    userValidator,
+    verifyUser,
+}
+
+
